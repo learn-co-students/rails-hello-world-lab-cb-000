57940bf9999e0ede28401ccd11715df00711ff48
Kirby Campbell <jkirbycampbell@gmail.com>
2018-09-06 20:05:19 +0000

    Done.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,9 @@ class StaticController < ApplicationController
 
 
   def hello_world
-     "hello_world"
+    #--Implicit doesn't need the below line - Since it looks in Static View Folder Automatically
+    #--and since this method is called hello_world it also finds the hello_world.html.erb file.
+     #"hello_world"
   end
 
 end
