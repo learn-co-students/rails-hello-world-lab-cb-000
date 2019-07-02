62bd7e69cf434a48192300aa4c923d40543546c2
jmoypace <jmoypace@gmail.com>
2019-07-02 21:10:04 +0000

    Done.

---
+++ app/views/helloworld/class Helloworld.rb
@@ -0,0 +1,2 @@
+class helloworld < ActiveRecord::base
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+
+get '/hello_world' => 'helloworld#home'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+get  '/hello_world' => 'hello_world#home'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
